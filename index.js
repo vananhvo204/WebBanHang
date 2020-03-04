3ba7e423477668563fea986b697b9ad4ce84132a
Võ Thị Vân Anh <49057693+vananhvo204@users.noreply.github.com>
2020-03-04 23:14:36 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+let express = require('express');
+let app = express();
+
+// Set Public Static Folder
+app.use(express.static(__dirname+'/public'));
+
+// Use View Engine
+let expressHbs=require('express-handlebars');
+let hbs=expressHbs.create({
+    extname:'hbs',
+    defaultLayout:'layout',
+    layoutsDir:__dirname+'/views/layouts/',
+    partialsDir:__dirname+'/views/partials/'
+});
+app.engine('hbs',hbs.engine);
+app.set('view engine','hbs');
+
+//Define your routes here
+app.get('/',(req, res)=>{
+    res.render('index');
+});
+
+app.get('/:page',(req, res)=>{
+    let banners = {
+        blog:'Our Blog',
+        category:'Shop Category',
+        cart:'Shopping Cart',
+        confirmation:'Order Confirmation',
+        contact:'Contact Us',
+        login:'Login/ Register',
+        register:'Register',
+        checkout:'Product Checkout'
+    };
+    let page=req.params.page;
+    res.render(page,{banner: banners[page]});
+});
+
+// Set Server Port & Start Server
+app.set('port', process.env.PORT || 5000);
+app.listen(app.get('port'), ()=>{
+    console.log(`Server is running at port ${app.get('port')}`);
+});
